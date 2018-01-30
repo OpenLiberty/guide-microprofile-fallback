3360a2a3881df3371e29ce47af9778fc071b806a
Grace Jansen <grace.jansen1@ibm.com>
2018-01-30 21:15:25 +0000

    adding everything

---
+++ finish/src/main/java/io/openliberty/guides/inventory/InventoryResource.java
@@ -0,0 +1,64 @@
+// tag::copyright[]
+/*******************************************************************************
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *******************************************************************************/
+// end::copyright[]
+
+// tag::config-methods[]
+package io.openliberty.guides.inventory;
+
+// CDI
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+
+// JSON-P
+import javax.json.JsonObject;
+
+// JAX-RS
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import io.openliberty.guides.common.JsonMessages;
+import io.openliberty.guides.inventory.InventoryConfig;
+
+import java.io.IOException;
+
+@RequestScoped
+@Path("hosts")
+public class InventoryResource {
+
+  @Inject InventoryManager manager;
+  @Inject InventoryConfig config;
+    
+    @GET
+    @Path("{hostname}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject getPropertiesForHost(@PathParam("hostname") String hostname) throws Exception{
+        //return manager.get(hostname);
+        // return config.isInMaintenance() ? throw new IOException() : manager.get(hostname);
+        if (config.isInvInMaintenance()) {
+            throw new Exception();
+        } 
+        else{
+            return manager.get(hostname);
+        }
+    }
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public JsonObject listContents() {
+    return manager.list();
+  }
+
+}
+
+// end::config-methods[]
