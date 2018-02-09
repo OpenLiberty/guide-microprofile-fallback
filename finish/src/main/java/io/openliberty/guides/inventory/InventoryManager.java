c16b8964c7db3899c429039d047f0fbc58ddde4d
Gkerta <gkerta.seferi@ibm.com>
2018-02-09 14:20:28 +0000

    Updated tests, reversed tests order

--- finish/src/main/java/io/openliberty/guides/inventory/InventoryManager.java
+++ finish/src/main/java/io/openliberty/guides/inventory/InventoryManager.java
@@ -17,20 +17,33 @@ package io.openliberty.guides.inventory;
 import java.io.IOException;
 import java.util.Properties;
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+
 import org.eclipse.microprofile.faulttolerance.Fallback;
 import org.eclipse.microprofile.faulttolerance.Retry;
 import io.openliberty.guides.common.JsonMessages;
 import io.openliberty.guides.inventory.client.SystemClient;
 import io.openliberty.guides.inventory.model.InventoryList;
+import io.openliberty.guides.system.SystemConfig;
 
 @ApplicationScoped
 public class InventoryManager {
 
   private InventoryList invList = new InventoryList();
+  private static int retryCounter = 0;
+  
+  @Inject
+  SystemConfig systemConfig;
 
   @Retry(retryOn = IOException.class, maxRetries = 3)
   @Fallback(fallbackMethod = "fallbackForGet")
   public Properties get(String hostname) throws IOException {
+    if (systemConfig.isInMaintenance()) {
+      retryCounter++;
+    }
     SystemClient systemClient = new SystemClient(hostname);
     if (systemClient.isResponseOk()) {
       Properties properties = systemClient.getContent();
@@ -45,7 +58,7 @@ public class InventoryManager {
     if (properties == null) {
       System.out.println("This is the Fallback method being called!!!");
       // return JsonMessages.SERVICE_UNREACHABLE.getJson(); - incorrect
-      JsonMessages.serviceInMaintenance("system"); //need to fix this msg
+      JsonMessages.serviceInMaintenance("Service is temporarily down for maintenance"); //need to fix this msg
     }
     return properties;
   }
@@ -53,5 +66,18 @@ public class InventoryManager {
   public InventoryList list() {
     return invList;
   }
+
+  public static JsonObject getRetryCounter() {
+    JsonObjectBuilder methods = Json.createObjectBuilder();
+    methods.add("getRetryCounter", retryCounter);
+    JsonObjectBuilder retries = Json.createObjectBuilder();
+    retries.add("Inventory", methods.build());
+    return retries.build();
+  }
+    
+  public static void resetRetryCounter() {
+    retryCounter = 0;
+  }
+  
 }
 // end::add_retry_fallback[]
