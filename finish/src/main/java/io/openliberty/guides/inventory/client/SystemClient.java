575cfad7c00ab5c1ce2827c14ae9b42d6158a001
Eveline Cai <evelinec@ca.ibm.com>
2018-02-13 02:22:07 -0500

    Integrated with latest systemclient changes from cdi app

--- finish/src/main/java/io/openliberty/guides/inventory/client/SystemClient.java
+++ finish/src/main/java/io/openliberty/guides/inventory/client/SystemClient.java
@@ -13,89 +13,50 @@
 package io.openliberty.guides.inventory.client;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.net.URI;
-import java.net.URL;
 import java.util.Properties;
-
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Invocation.Builder;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 public class SystemClient {
 
   // Constants for building URI to the system service.
-  private static final int DEFAULT_PORT = Integer.valueOf(System.getProperty("default.http.port"));
-  private static final String SYSTEM_PROPERTIES = "/system/properties";
-  private static final String PROTOCOL = "http";
-  private static final String SECURED_PROTOCOL = "https";
+  private final int DEFAULT_PORT = Integer.valueOf(System.getProperty("default.http.port"));
+  private final String SYSTEM_PROPERTIES = "/system/properties";
+  private final String PROTOCOL = "http";
 
   private String url;
-  private boolean status;
-  private Properties content;
+  private Builder clientBuilder;
 
-  /**
-   * Used by the following guide(s): CDI, MP-METRICS, FAULT-TOLERANCE
-   */
-  public SystemClient(String hostname) throws IOException {
-    init(PROTOCOL, hostname, DEFAULT_PORT, null);
+  // Used by the following guide(s): CDI, MP-METRICS, FAULT-TOLERANCE
+  public void init(String hostname) {
+    this.initHelper(hostname, DEFAULT_PORT);
   }
 
-  /**
-   * Used by the following guide(s): MP-CONFIG, MP-HEALTH
-   */
-  /*public SystemClient(String hostname, int port) throws IOException {
-    init(PROTOCOL, hostname, port, null);
-  }*/
-
-  /**
-   * Used by the following guide(s): MP-JWT
-   */
-  /*public SystemClient(String hostname, String authHeader) throws IOException {
-    init(SECURED_PROTOCOL, hostname, DEFAULT_PORT, authHeader);
-  }*/
-
-  /**
-   * Helper function to set the attributes.
-   */
-  private void init(String protocol, String hostname, int port, String authHeader) throws IOException {
-    this.setUrl(protocol, hostname, port, SYSTEM_PROPERTIES);
-    this.setStatus();
-    if (this.status) {
-      this.setContent(authHeader);
-    }
+  // Used by the following guide(s): MP-CONFIG, MP-HEALTH
+  public void init(String hostname, int port) {
+    this.initHelper(hostname, port);
   }
 
-  /**
-   * URL getter
-   */
-  public String getUrl() {
-    return this.url;
+  // Helper method to set the attributes.
+  private void initHelper(String hostname, int port) {
+    this.url = buildUrl(PROTOCOL, hostname, port, SYSTEM_PROPERTIES);
+    this.clientBuilder = buildClientBuilder(this.url);
   }
 
-  /**
-   * Status getter
-   */
-  public boolean isResponseOk() {
-    return this.status;
+  // Wrapper function that gets properties
+  public Properties getProperties() throws IOException {
+    return getPropertiesHelper(this.clientBuilder);
   }
 
+  // tag::doc[]
   /**
-   * Content getter
-   */
-  public Properties getContent() {
-    return this.content;
-  }
-
-  /**
-   * <p>
-   * Builds the URI string to the system service for a particular host. This is
-   * just a helper method.
-   * </p>
-   *
+   * Builds the URI string to the system service for a particular host.
    * @param protocol
    *          - http or https.
    * @param host
@@ -106,49 +67,44 @@ public class SystemClient {
    *          - Note that the path needs to start with a slash!!!
    * @return String representation of the URI to the system properties service.
    */
-  private void setUrl(String protocol, String host, int port, String path) {
+  // end::doc[]
+  protected String buildUrl(String protocol, String host, int port, String path) {
     try {
       URI uri = new URI(protocol, null, host, port, path, null, null);
-      this.url = uri.toString();
+      return uri.toString();
     } catch (Exception e) {
       System.out.println("URISyntaxException");
-      this.url = null;
+      return null;
     }
   }
 
-  /**
-   * Status setter
-   */
-  private void setStatus() throws IOException {
-    URL target = new URL(this.url);
-    HttpURLConnection http = (HttpURLConnection) target.openConnection();
-    http.setConnectTimeout(50);
-    int response = http.getResponseCode();
-    if (response == 503) throw new IOException();
-    this.status = (response != 200) ? false : true;
-    /*try {
-      URL target = new URL(this.url);
-      HttpURLConnection http = (HttpURLConnection) target.openConnection();
-      http.setConnectTimeout(50);
-      int response = http.getResponseCode();
-      this.status = (response != 200) ? false : true;
+  // Method that creates the client builder
+  protected Builder buildClientBuilder(String urlString) {
+    try {
+      Client client = ClientBuilder.newClient();
+      Builder builder = client.target(urlString).request();
+      return builder.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON);
     } catch (Exception e) {
-      this.status = false;
-    }*/
-  }
-
-  /**
-   * Content setter
-   */
-  private void setContent(String authHeader) {
-    Client client = ClientBuilder.newClient();
-    Builder builder = client.target(this.url).request();
-    builder.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON);
-    if (authHeader != null) {
-      builder.header(HttpHeaders.AUTHORIZATION, authHeader);
+      System.out.println("ClientBuilderException");
+      return null;
     }
-    Response response = builder.build("GET").invoke();
-    this.content = response.readEntity(Properties.class);
+  }
+  
+  protected Properties getPropertiesHelper(Builder builder) throws IOException {
+    try {
+      Response response = builder.get();
+      int status = response.getStatus();
+      if (status == Status.SERVICE_UNAVAILABLE.getStatusCode()) {
+        throw new IOException();
+      } else if (status == Status.OK.getStatusCode()) {
+        return response.readEntity(Properties.class);
+      } else {
+        System.out.println("Response Status is not OK.");
+      }
+    } catch (RuntimeException e) {
+      System.out.println("Runtime exception: " + e.getMessage());
+    }
+    return null;
   }
 
 }
