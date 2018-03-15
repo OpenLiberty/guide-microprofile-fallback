81b49f7ad8da438e37a0ee00a777e415d41d1eb2
Eveline Cai <evelinec@ca.ibm.com>
2018-03-15 12:00:05 -0400

    Refreshed the start directory

---
+++ start/src/main/java/io/openliberty/guides/inventory/client/SystemClient.java
@@ -0,0 +1,148 @@
+// tag::copyright[]
+/*******************************************************************************
+ * Copyright (c) 2017, 2018 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *******************************************************************************/
+// end::copyright[]
+// tag::throw_IOException[]
+package io.openliberty.guides.inventory.client;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation.Builder;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import java.util.Properties;
+import java.io.IOException;
+import java.net.URI;
+
+public class SystemClient {
+
+    // tag::javadoc[]
+    /**
+     * Constants for building URI to the system service.
+     */
+    // end::javadoc[]
+    private final int DEFAULT_PORT = Integer.valueOf(System.getProperty("default.http.port"));
+    private final String SYSTEM_PROPERTIES = "/system/properties";
+    private final String PROTOCOL = "http";
+
+    private URI uri;
+    private Builder clientBuilder;
+
+    public void init(String hostname) {
+        this.initHelper(hostname, DEFAULT_PORT);
+    }
+
+    public void init(String hostname, int port) {
+        this.initHelper(hostname, port);
+    }
+
+    // tag::javadoc[]
+    /**
+     * Helper method to set the attributes.
+     *
+     * @param hostname
+     * @param port
+     */
+    // end::javadoc[]
+    private void initHelper(String hostname, int port) {
+        this.uri = buildUri(PROTOCOL, hostname, port, SYSTEM_PROPERTIES);
+        this.clientBuilder = buildClientBuilder();
+    }
+
+    // tag::javadoc[]
+    /**
+     * Wrapper function that gets properties
+     *
+     * @return
+     * @throws IOException
+     */
+    // end::javadoc[]
+    public Properties getProperties() throws IOException {
+        return getPropertiesHelper(this.clientBuilder);
+    }
+
+    // tag::javadoc[]
+    /**
+     * Builds the URI string to the system service for a particular host.
+     * 
+     * @param protocol
+     *            - http or https.
+     * @param host
+     *            - name of host.
+     * @param port
+     *            - port number.
+     * @param path
+     *            - Note that the path needs to start with a slash!!!
+     * @return URI of the system properties service.
+     */
+    // end::javadoc[]
+    protected URI buildUri(String protocol, String host, int port,
+            String path) {
+        try {
+            return new URI(protocol, null, host, port, path, null, null);
+        } catch (Exception e) {
+            System.err.println("Exception thrown while building the URL: "
+                    + e.getMessage());
+            return null;
+        }
+    }
+
+    // tag::javadoc[]
+    /**
+     * Method that creates the client builder
+     *
+     * @param urlString
+     * @return
+     */
+    // end::javadoc[]
+    protected Builder buildClientBuilder() {
+        try {
+            Client client = ClientBuilder.newClient();
+            Builder builder = client.target(this.uri).request();
+            return builder.header(HttpHeaders.CONTENT_TYPE,
+                                  MediaType.APPLICATION_JSON);
+        } catch (Exception e) {
+            System.err.println("Exception thrown while building the client: "
+                    + e.getMessage());
+            return null;
+        }
+    }
+
+    // tag::javadoc[]
+    /**
+     * Helper method that processes the request
+     *
+     * @param builder
+     * @return
+     * @throws IOException
+     */
+    // end::javadoc[]
+    protected Properties getPropertiesHelper(Builder builder)
+            throws IOException {
+        try {
+            Response response = builder.get();
+            int status = response.getStatus();
+            if (status == Status.SERVICE_UNAVAILABLE.getStatusCode()) {
+                throw new IOException();
+            } else if (status == Status.OK.getStatusCode()) {
+                return response.readEntity(Properties.class);
+            } else {
+                System.err.println("Response Status is not OK.");
+            }
+        } catch (RuntimeException e) {
+            System.err.println("Runtime exception: " + e.getMessage());
+        }
+        return null;
+    }
+}
+// end::throw_IOException[]
