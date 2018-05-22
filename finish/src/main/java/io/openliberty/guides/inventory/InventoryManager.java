56272d30468a4218501f488a1635f3d2fb853bb8
Justine Chen <justine.chen@ibm.com>
2018-05-22 15:22:14 -0400

    Finish folder passes tests with REST MP Client

--- finish/src/main/java/io/openliberty/guides/inventory/InventoryManager.java
+++ finish/src/main/java/io/openliberty/guides/inventory/InventoryManager.java
@@ -16,29 +16,42 @@ package io.openliberty.guides.inventory;
 
 import java.io.IOException;
 import java.util.Properties;
+import javax.inject.Inject;
 
 import javax.enterprise.context.ApplicationScoped;
 
 import org.eclipse.microprofile.faulttolerance.Fallback;
 import io.openliberty.guides.inventory.client.SystemClient;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 import io.openliberty.guides.inventory.model.InventoryList;
 
 @ApplicationScoped
 public class InventoryManager {
 
     private InventoryList invList = new InventoryList();
+    private InventoryUtils invUtils = new InventoryUtils();
+
+    @Inject
+    @RestClient
+    private SystemClient defaultRestClient;
 
     @Fallback(fallbackMethod = "fallbackForGet")
     public Properties get(String hostname) throws IOException {
-        SystemClient systemClient = new SystemClient();
-        systemClient.init(hostname);
-        Properties properties = systemClient.getProperties();
+        // SystemClient systemClient = new SystemClient();
+        // systemClient.init(hostname);
+        // Properties properties = systemClient.getProperties();
+        Properties properties = null;
+        if (hostname.equals("localhost")) {
+          properties = invUtils.getPropertiesWithDefaultHostName(defaultRestClient);
+        } else {
+          properties = invUtils.getPropertiesWithGivenHostName(hostname);
+        }
 
         if (properties != null) {
-            invList.addToInventoryList(hostname, properties);
+          invList.addToInventoryList(hostname, properties);
         }
         return properties;
-    }
+        }
 
     public Properties fallbackForGet(String hostname) {
         Properties properties = invList.findHost(hostname);
--- finish/src/main/java/io/openliberty/guides/inventory/client/SystemClient.java
+++ finish/src/main/java/io/openliberty/guides/inventory/client/SystemClient.java
@@ -10,139 +10,28 @@
  *     IBM Corporation - Initial implementation
  *******************************************************************************/
 // end::copyright[]
-// tag::throw_IOException[]
+// tag::client[]
 package io.openliberty.guides.inventory.client;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Invocation.Builder;
-import javax.ws.rs.core.HttpHeaders;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 import java.util.Properties;
 import java.io.IOException;
-import java.net.URI;
+import javax.enterprise.context.Dependent;
+import javax.ws.rs.ProcessingException;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import org.eclipse.microprofile.rest.client.annotation.RegisterProvider;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
-public class SystemClient {
-
-    // tag::javadoc[]
-    /**
-     * Constants for building URI to the system service.
-     */
-    // end::javadoc[]
-    private final int DEFAULT_PORT = Integer.valueOf(System.getProperty("default.http.port"));
-    private final String SYSTEM_PROPERTIES = "/system/properties";
-    private final String PROTOCOL = "http";
-
-    private URI uri;
-    private Builder clientBuilder;
-
-    public void init(String hostname) {
-        this.initHelper(hostname, DEFAULT_PORT);
-    }
-
-    public void init(String hostname, int port) {
-        this.initHelper(hostname, port);
-    }
-
-    // tag::javadoc[]
-    /**
-     * Helper method to set the attributes.
-     *
-     * @param hostname
-     * @param port
-     */
-    // end::javadoc[]
-    private void initHelper(String hostname, int port) {
-        this.uri = buildUri(PROTOCOL, hostname, port, SYSTEM_PROPERTIES);
-        this.clientBuilder = buildClientBuilder();
-    }
-
-    // tag::javadoc[]
-    /**
-     * Wrapper function that gets properties
-     *
-     * @return
-     * @throws IOException
-     */
-    // end::javadoc[]
-    public Properties getProperties() throws IOException {
-        return getPropertiesHelper(this.clientBuilder);
-    }
-
-    // tag::javadoc[]
-    /**
-     * Builds the URI string to the system service for a particular host.
-     * 
-     * @param protocol
-     *            - http or https.
-     * @param host
-     *            - name of host.
-     * @param port
-     *            - port number.
-     * @param path
-     *            - Note that the path needs to start with a slash!!!
-     * @return URI of the system properties service.
-     */
-    // end::javadoc[]
-    protected URI buildUri(String protocol, String host, int port,
-            String path) {
-        try {
-            return new URI(protocol, null, host, port, path, null, null);
-        } catch (Exception e) {
-            System.err.println("Exception thrown while building the URL: "
-                    + e.getMessage());
-            return null;
-        }
-    }
-
-    // tag::javadoc[]
-    /**
-     * Method that creates the client builder
-     *
-     * @param urlString
-     * @return
-     */
-    // end::javadoc[]
-    protected Builder buildClientBuilder() {
-        try {
-            Client client = ClientBuilder.newClient();
-            Builder builder = client.target(this.uri).request();
-            return builder.header(HttpHeaders.CONTENT_TYPE,
-                                  MediaType.APPLICATION_JSON);
-        } catch (Exception e) {
-            System.err.println("Exception thrown while building the client: "
-                    + e.getMessage());
-            return null;
-        }
-    }
-
-    // tag::javadoc[]
-    /**
-     * Helper method that processes the request
-     *
-     * @param builder
-     * @return
-     * @throws IOException
-     */
-    // end::javadoc[]
-    protected Properties getPropertiesHelper(Builder builder)
-            throws IOException {
-        try {
-            Response response = builder.get();
-            int status = response.getStatus();
-            if (status == Status.SERVICE_UNAVAILABLE.getStatusCode()) {
-                throw new IOException();
-            } else if (status == Status.OK.getStatusCode()) {
-                return response.readEntity(Properties.class);
-            } else {
-                System.err.println("Response Status is not OK.");
-            }
-        } catch (RuntimeException e) {
-            System.err.println("Runtime exception: " + e.getMessage());
-        }
-        return null;
-    }
-}
-// end::throw_IOException[]
+// tag::annotations[]
+@Dependent
+@RegisterRestClient
+@RegisterProvider(UnknownUrlExceptionMapper.class)
+@Path("/properties")
+public interface SystemClient {
+  // end::annotations[]
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public Properties getProperties() throws UnknownUrlException, IOException, ProcessingException;
+// end::client[]
