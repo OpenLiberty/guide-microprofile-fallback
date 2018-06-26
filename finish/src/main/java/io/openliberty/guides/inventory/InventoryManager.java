10fde23d3f73d44159f27974b080a533b9b5add5
Justine Chen <justine.chen@ibm.com>
2018-06-26 10:28:21 -0400

    Removed defaultRestClient

--- finish/src/main/java/io/openliberty/guides/inventory/InventoryManager.java
+++ finish/src/main/java/io/openliberty/guides/inventory/InventoryManager.java
@@ -16,13 +16,8 @@ package io.openliberty.guides.inventory;
 
 import java.io.IOException;
 import java.util.Properties;
-import javax.inject.Inject;
-
 import javax.enterprise.context.ApplicationScoped;
-
 import org.eclipse.microprofile.faulttolerance.Fallback;
-import io.openliberty.guides.inventory.client.SystemClient;
-import org.eclipse.microprofile.rest.client.inject.RestClient;
 import io.openliberty.guides.inventory.model.InventoryList;
 
 @ApplicationScoped
@@ -31,18 +26,9 @@ public class InventoryManager {
   private InventoryList invList = new InventoryList();
   private InventoryUtils invUtils = new InventoryUtils();
 
-  @Inject
-  @RestClient
-  private SystemClient defaultRestClient;
-
   @Fallback(fallbackMethod = "fallbackForGet")
   public Properties get(String hostname) throws IOException {
-    Properties properties = null;
-    if (hostname.equals("localhost")) {
-      properties = invUtils.getPropertiesWithDefaultHostName(defaultRestClient);
-    } else {
-      properties = invUtils.getPropertiesWithGivenHostName(hostname);
-    }
+    Properties properties = invUtils.getPropertiesWithGivenHostName(hostname);
 
     if (properties != null) {
       invList.addToInventoryList(hostname, properties);
